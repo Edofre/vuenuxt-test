109cea1565550058075005f7187dce6ee2be782b
Edo Freriks <edofre@gmail.com>
2018-04-27 23:05:55 +0200

    Include bulma + font-awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,18 +5,18 @@ module.exports = {
   head: {
     title: 'vuenuxt-test',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Testing Nuxt.js ' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Testing Nuxt.js '}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#3B8070'},
   /*
   ** Build configuration
   */
@@ -24,7 +24,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -34,5 +34,10 @@ module.exports = {
         })
       }
     }
-  }
-}
+  },
+  css: [
+    // node.js module but we specify the pre-processor
+    {src: '~assets/scss/main.scss', lang: 'sass'},
+    {src: 'font-awesome/scss/font-awesome.scss', lang: 'scss'},
+  ],
+};
