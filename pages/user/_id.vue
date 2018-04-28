2c828761908babb10d5e409a71c39a6f6972ad61
Edo Freriks <edofre@gmail.com>
2018-04-28 23:01:23 +0200

    routing

---
+++ pages/user/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        User
+      </h1>
+      <p class="subtitle">
+        {{ test }}
+        {{ $route.params.id }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        test: 'Test',
+      }
+    },
+    validate({params}) {
+      // Must be a number
+      return /^\d+$/.test(params.id)
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/one.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        User
+      </h1>
+      <p class="subtitle">
+        One
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
