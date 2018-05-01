3f34e4fe3dd9291858f30e714dc93eabdc4374ed
Edo Freriks <edofre@gmail.com>
2018-05-01 23:04:43 +0200

    Axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,18 @@ module.exports = {
   */
   loading: {color: '#3B8070'},
   /*
+  ** Modules
+   */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios
+   */
+  axios: {
+    // proxyHeaders: false
+  },
+  /*
   ** Middleware configuration
   */
   router: {
