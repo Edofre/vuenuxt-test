8d11ebc9b6a850d9228475699766f69b39e65cb6
Edo Freriks <edofre@gmail.com>
2018-05-02 23:05:24 +0200

    Image assets

---
+++ pages/assets.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Assets
+      </h1>
+    </div>
+
+    <div class="container">
+      <div class="notification">
+        <img src="~/assets/images/nuxt.png">
+      </div>
+
+    </div>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Testing footer</strong>
+            <nuxt-link class="footer-link" to="/">
+              <i class="fa fa-home" aria-hidden="true"></i>
+            </nuxt-link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        title: 'Test',
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
