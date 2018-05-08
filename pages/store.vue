9087d522a1ec8909368df6b31815330d70dea1ff
Edo Freriks <edofre@gmail.com>
2018-05-08 22:07:02 +0200

    Classic store

---
+++ pages/store.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Store
+      </h1>
+    </div>
+
+    <button @click="$store.commit('increment')">{{ $store.state.counter }}</button>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Testing footer</strong>
+            <nuxt-link class="footer-link" to="/">
+              <i class="fa fa-home" aria-hidden="true"></i>
+            </nuxt-link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
